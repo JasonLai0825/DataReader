7a73481c224c2ed8569006e5222b61d077ff57ab
JasonLai <jasongood3312@gmail.com>
2018-12-13 15:57:52 +0800

    1. 加入 PHPExcel 套件 2. 加入基本目錄架構
    Signed-off-by: JasonLai <jasongood3312@gmail.com>

--- lib/Config/config.php
+++ lib/Config/config.php
@@ -1,7 +1,8 @@
 <?php
-    $config = array(
-        "Target_directory" => "W:\\ftp\Asadal\\20140715",
-        "Excel_fileName" => "雲端文創 Asadal 圖片資料",
-        "Excel_sheetNames" => array("倉儲"),
-        "Excel_usedRow" => array('A' => '檔案名稱', 'B' => '完整路徑')
-    );
+$config = array(
+    /* settings of excel */
+    "Target_directory" => "W:\\ftp\Asadal\\20140715",
+    "Excel_fileName" => "雲端文創 Asadal 圖片資料",
+    "Excel_sheetNames" => array("倉儲"),
+    "Excel_usedRow" => array('A' => '檔案名稱', 'B' => '完整路徑')
+);
